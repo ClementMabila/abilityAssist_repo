89898859586d9820c63b11818482c7c9a3add760
Clement <Clementmabila95@gmail.com>
2024-04-15 03:38:39 +0200

    secon commit

--- frontend/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-    return <div>
-        <h1>404 Not Found</h1>
-        <p>The page ypu're looking for doesn't exist.</p>
-        <p>Ability Assis Officical</p>
-    </div>
-}
-
-export default NotFound
